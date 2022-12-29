826afea5c303e1b65ef12a89207dd1afaae4c475
Atmiya Jadvani <atmiya@my.yorku.ca>
2022-12-29 14:01:01 -0500

    saving

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,4 +1,7 @@
-const { Configuration, OpenAIApi } = require('openai');
+const {
+    Configuration,
+    OpenAIApi
+} = require('openai');
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -6,37 +9,42 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
-const { prompt, size } = req.body;
-
-const imageSize =
-    size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
-
-try {
-    const response = await openai.createImage({
+    const {
         prompt,
-        n: 1,
-        size: imageSize,
-    });
+        size
+    } = req.body;
 
-    const imageUrl = response.data.data[0].url;
+    const imageSize =
+        size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
 
-    res.status(200).json({
-        success: true,
-        data: imageUrl,
-    });
-} catch (error) {
-    if (error.response) {
-        console.log(error.response.status);
-        console.log(error.response.data);
-    } else {
-        console.log(error.message);
-    }
+    try {
+        const response = await openai.createImage({
+            prompt,
+            n: 1,
+            size: imageSize,
+        });
 
-    res.status(400).json({
-        success: false,
-        error: 'The image could not be generated',
-    });
+        const imageUrl = response.data.data[0].url;
+
+        res.status(200).json({
+            success: true,
+            data: imageUrl,
+        });
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+        } else {
+            console.log(error.message);
+        }
+
+        res.status(400).json({
+            success: false,
+            error: 'The image could not be generated',
+        });
     }
 };
 
-module.exports = { generateImage };
+module.exports = {
+    generateImage
+};

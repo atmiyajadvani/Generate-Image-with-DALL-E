8ff63efada3794a9943caf09660ed48571c98728
Atmiya Jadvani <atmiya@my.yorku.ca>
2022-12-29 12:45:45 -0500

    gitignore env

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,4 +1,4 @@
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -6,33 +6,37 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
-    try {
-        const response = await openai.createImage({
-            prompt: 'Polar bear on ice',
-            n: 1,
-            size: '512x512',
-        });
+const { prompt, size } = req.body;
 
-        const imageURL = response.data.data[0].url;
+const imageSize =
+    size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
 
-        res.status(200).json({ 
-            success: true, 
-            data: imageURL
-        });
-    } catch (error) {
+try {
+    const response = await openai.createImage({
+        prompt,
+        n: 1,
+        size: imageSize,
+    });
 
-        if (error.response) {
-            console.log(error.response.status);
-            console.log(error.response.data);
-        } else {
-            console.log(error.message);
-        }
+    const imageUrl = response.data.data[0].url;
 
-        res.status(400).json({ 
-            success: false, 
-            error: 'The image cannot be generated.'
-        });
+    res.status(200).json({
+        success: true,
+        data: imageUrl,
+    });
+} catch (error) {
+    if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+    } else {
+        console.log(error.message);
+    }
+
+    res.status(400).json({
+        success: false,
+        error: 'The image could not be generated',
+    });
     }
 };
 
-module.exports = {generateImage};
+module.exports = { generateImage };
